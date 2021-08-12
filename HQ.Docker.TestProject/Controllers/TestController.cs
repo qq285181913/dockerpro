f483717fd25c231f6b7100197467f794007d8159
wangminhua <beauty2006wmh@qq.com>
2021-08-12 13:59:48 +0800

    第一次发布
    第一次发布

---
+++ HQ.Docker.TestProject/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HQ.Docker.TestProject.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> FirstDockerTest()
+        {
+            return "我是一只小小鸟";
+        }
+    }
+}
